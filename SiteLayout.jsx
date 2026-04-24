5ca8dee2b234df6f755ec60a1f62b47de3c4d2a5
kknktpq4kn-design <kknktpq4kn@privaterelay.appleid.com>
2026-04-24 14:37:27 +0200

    Add SiteLayout component with header and footer

---
+++ SiteLayout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function SiteLayout() {
+  return (
+    <div className="min-h-screen flex flex-col font-main"
+      style={{ background: "linear-gradient(180deg, #c8dff0 0%, #dde9f3 30%, #e8f0f6 60%, #c8dff0 100%)" }}>
+      <Header />
+      <div className="flex-1"><Outlet /></div>
+      <Footer />
+    </div>
+  );
+}
