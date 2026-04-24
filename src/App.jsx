95a57d7e1bb325e15163a243d755f77ab275047f
kknktpq4kn-design <kknktpq4kn@privaterelay.appleid.com>
2026-04-24 17:24:37 +0200

    Remove Toaster component from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-//import { Toaster } from "@/components/ui/toaster"//
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -44,10 +43,12 @@ function App() {
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
-        <Router><AuthenticatedApp /></Router>
-        <Toaster />
+        <Router>
+          <AuthenticatedApp />
+        </Router>
       </QueryClientProvider>
     </AuthProvider>
   );
 }
+
 export default App;
